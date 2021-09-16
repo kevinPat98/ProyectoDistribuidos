0b0959549d8428861f587bbfdbf39316ac59eb0f
nico19989 <57466420+nico19989@users.noreply.github.com>
2021-09-16 14:03:01 -0500

    Act 1

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req,res) => {
-    res.render('index.html', { title : 'primero' });
+    res.render('index.html', { title : 'UPTC' });
 });
-
 router.get('/contact', (req,res) => {
     res.render('contact.html', { title : 'contact' });
 });
-
+router.get('/soga', (req,res) => {
+    res.render('soga.html', { title : 'soga' });
+});
+router.get('/index', (req,res) => {
+    res.render('index.html', { title : 'Inicio' });
+});
 module.exports = router;
